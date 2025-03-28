346d0164ca07bb472cd2a0d1aa4b86dfe0cce8b3
mazzuzugaib <mazzuzugaib@gmail.com>
2025-03-28 12:05:05 -0300

    criando tabela do usuario
    criei a tabela onde serao cadastrados os usuarios, ja deixei 3 por padrao. Acrescentei tambem comentarios em algumas partes para consulta previa.

--- db_musicas.sql
+++ db_musicas.sql
@@ -1,5 +1,7 @@
 create database play_musica;
 use play_musica;
+
+/*tabela das musicas que serao cadastradas*/
 create table musica(
 	id int primary key auto_increment not null,
     titulo varchar(50) not null,
@@ -12,3 +14,16 @@ values ('Saturday Night', 'Elton John', 'Rock'),
     ('Shape of You', 'Ed Sheeran', 'Pop');  
 select * from musica;
 delete from musica where id = 1;
+
+/*tabela dos usuarios*/
+create table usuario(
+	id int primary key auto_increment not null,
+    nome_us varchar(40) not null,
+    login_us varchar(10) not null,
+    senha_us varchar(10) not null);
+    
+/*adicionando usuarios na tabela*/
+insert into usuario(nome_us, login_us, senha_us)
+values ('Lorem Ipsum', 'lorem', 'ipsum'),
+('Dolor sit', 'Dolor', 'Sit'),
+('Amet Consectetur', 'Amet', 'Consec');
